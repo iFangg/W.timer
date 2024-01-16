186e946a9def9ba2a152a1bacb55a4c56c485467
Ivan Fang <iFang.py@gmail.com>
2024-01-16 23:03:46 +1100

    Transferring progress

---
+++ app/src/main/java/com/example/timer/MainActivity.kt
@@ -0,0 +1,211 @@
+package com.example.timer
+
+import android.app.AlarmManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.os.CountDownTimer
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import com.example.timer.databinding.ActivityMainBinding
+import com.example.timer.util.BindingUtil
+import com.example.timer.util.PrefUtil
+import java.util.Calendar
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityMainBinding
+
+    companion object {
+        fun setAlarm(context: Context, currSec: Long, remainSec: Long): Long {
+            val wakeUp = (currSec + remainSec) * 1000
+            val manager = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
+            val intent = Intent(context, TimerExpiredReceiver::class.java)
+            val pendingIntent = PendingIntent.getBroadcast(context, 0, intent, 0)
+            manager.setExact(AlarmManager.RTC_WAKEUP, wakeUp, pendingIntent)
+            PrefUtil.setAlarmSetTime(nowSec, context)
+            return wakeUp
+        }
+
+        val nowSec: Long
+            get() = Calendar.getInstance().timeInMillis / 1000
+    }
+
+    enum class TimerState {
+        Stopped, Paused, Running
+    }
+
+    private lateinit var timer: CountDownTimer
+    private var timerLengthSeconds: Long = 0L
+    private var timerState: TimerState = TimerState.Stopped
+    private var secondsRemaining: Long = 0L
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = BindingUtil.inflateMainBinding(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+
+//        val navController = findNavController(R.id.nav_host_fragment_content_main)
+//        appBarConfiguration = AppBarConfiguration(navController.graph)
+//        setupActionBarWithNavController(navController, appBarConfiguration)
+
+        supportActionBar?.setIcon(R.drawable.ic_timer)
+        supportActionBar?.title = " Timer"
+
+        binding.fabPlay.setOnClickListener{v ->
+            startTimer()
+            timerState = TimerState.Running
+            updateButtons()
+        }
+
+        binding.fabPause.setOnClickListener{v ->
+            timer.cancel()
+            timerState = TimerState.Paused
+            updateButtons()
+        }
+
+        binding.fabStop.setOnClickListener{v ->
+            timer.cancel()
+            onTimerFinished()
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        initTimer()
+
+        // todo... idk
+    }
+
+    override fun onPause() {
+        super.onPause()
+
+        if (timerState == TimerState.Running) {
+            timer.cancel()
+            // todo...
+        } else if (timerState == TimerState.Paused) {
+            // todo...
+        }
+
+        PrefUtil.setPrevLenSeconds(timerLengthSeconds, this)
+        PrefUtil.setSecondsRemaining(secondsRemaining, this)
+        PrefUtil.setTimerState(timerState, this)
+    }
+
+    private fun initTimer() {
+        timerState = PrefUtil.getTimerState(this)
+        if (timerState == TimerState.Stopped) {
+            setNewTimerLength()
+        } else {
+            setPreviousTimerLength()
+        }
+
+        secondsRemaining = if (timerState == TimerState.Stopped) timerLengthSeconds else PrefUtil.getSecondsRemaining(this)
+        // todo...
+        if (timerState == TimerState.Running) startTimer()
+
+        updateButtons()
+        updateCountdownUI()
+    }
+
+    private fun onTimerFinished() {
+        timerState = TimerState.Stopped
+        setNewTimerLength()
+//        progress_countdown.progress = 0
+
+        PrefUtil.setSecondsRemaining(timerLengthSeconds, this)
+        secondsRemaining = timerLengthSeconds
+
+        updateButtons()
+        updateCountdownUI()
+    }
+
+    private fun startTimer() {
+        timerState = TimerState.Running
+
+        timer = object : CountDownTimer(secondsRemaining * 1000, 1000) {
+            override fun onFinish() = onTimerFinished()
+            override fun onTick(msTilDone: Long) {
+                secondsRemaining = msTilDone / 1000
+//                updateCountdown()
+            }
+        }.start()
+    }
+
+    private fun setNewTimerLength() {
+        val lenMin = PrefUtil.getTimerLen(this)
+        timerLengthSeconds = (lenMin * 60L)
+//        progress_countdown.max = timerLengthSeconds.toInt()
+    }
+
+    private fun setPreviousTimerLength() {
+        timerLengthSeconds = PrefUtil.getPrevTimerLenSeconds(this)
+//        progress_countdown.max = timerLengthSeconds.toInt()
+    }
+
+    private fun updateCountdownUI() {
+        val minTilFin = secondsRemaining / 60
+        val secInMinTilFin = secondsRemaining - minTilFin * 60
+        val secStr = secInMinTilFin.toString()
+
+        val bindingCont = BindingUtil.inflateContentBinding(layoutInflater)
+        val countdownText = getString(R.string.countdown_text, minTilFin, secStr)
+        bindingCont.textViewCountDown.text = countdownText
+//          progress_countdown.progress = (timerLengthSeconds - secondsRemaining).toInt() --> I don't have a progress bar
+    }
+
+    private fun updateButtons() {
+        // refactor?
+        when (timerState) {
+            TimerState.Running -> {
+                binding.fabPlay.isEnabled = false
+                binding.fabPause.isEnabled = true
+                binding.fabStop.isEnabled = true
+            }
+            TimerState.Paused -> {
+                binding.fabPlay.isEnabled = true
+                binding.fabPause.isEnabled = false
+                binding.fabStop.isEnabled = true
+            }
+            TimerState.Stopped -> {
+                binding.fabPlay.isEnabled = true
+                binding.fabPause.isEnabled = true
+                binding.fabStop.isEnabled = false
+            }
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+//    override fun onSupportNavigateUp(): Boolean {
+//        val navController = findNavController(R.id.nav_host_fragment_content_main)
+//        return navController.navigateUp(appBarConfiguration)
+//                || super.onSupportNavigateUp()
+//    }
+}
---
+++ app/src/main/java/com/example/timer/util/PrefUtil.kt
@@ -0,0 +1,68 @@
+package com.example.timer.util
+
+import android.content.Context
+import androidx.preference.PreferenceManager
+import com.example.timer.MainActivity
+
+class PrefUtil {
+    companion object {
+        fun getTimerLen(context: Context): Int {
+            // placeholder
+            return 1;
+        }
+
+        private const val PREVIOUS_TIMER_LEN_SEC_ID = "com.example.timer.previous_timer_len_id"
+
+        fun getPrevTimerLenSeconds(context: Context): Long {
+            val preferences = androidx.preference.PreferenceManager.getDefaultSharedPreferences(context)
+            return preferences.getLong(PREVIOUS_TIMER_LEN_SEC_ID, 0)
+        }
+
+        fun setPrevLenSeconds(seconds: Long, context: Context) {
+            val editor = androidx.preference.PreferenceManager.getDefaultSharedPreferences(context).edit()
+            editor.putLong(PREVIOUS_TIMER_LEN_SEC_ID, seconds)
+            editor.apply()
+        }
+
+        private const val TIMER_STATE_ID = "com.example.timer.timer_state"
+
+        fun getTimerState(context: Context): MainActivity.TimerState {
+            val preferences = androidx.preference.PreferenceManager.getDefaultSharedPreferences(context)
+            val ordinal = preferences.getInt(TIMER_STATE_ID, 0)
+            return MainActivity.TimerState.entries[ordinal]
+        }
+
+        fun setTimerState(state: MainActivity.TimerState, context: Context) {
+            val editor = androidx.preference.PreferenceManager.getDefaultSharedPreferences(context).edit()
+            val ordinal = state.ordinal
+            editor.putInt(TIMER_STATE_ID, ordinal)
+            editor.apply()
+        }
+
+        private const val SECONDS_REMAINING_ID = "com.example.timer.seconds_remaining"
+
+        fun getSecondsRemaining(context: Context): Long {
+            val preferences = androidx.preference.PreferenceManager.getDefaultSharedPreferences(context)
+            return preferences.getLong(SECONDS_REMAINING_ID, 0)
+        }
+
+        fun setSecondsRemaining(seconds: Long, context: Context) {
+            val editor = androidx.preference.PreferenceManager.getDefaultSharedPreferences(context).edit()
+            editor.putLong(SECONDS_REMAINING_ID, seconds)
+            editor.apply()
+        }
+
+        private const val ALARM_SET_TIME_ID = "com.example.timer.backgrounded_time"
+
+        fun getAlarmSetTime(context: Context): Long {
+            val preferences = PreferenceManager.getDefaultSharedPreferences(context)
+            return preferences.getLong(ALARM_SET_TIME_ID, 0)
+        }
+
+        fun setAlarmSetTime(time: Long, context: Context) {
+            val editor = androidx.preference.PreferenceManager.getDefaultSharedPreferences(context).edit()
+            editor.putLong(ALARM_SET_TIME_ID, time)
+            editor.apply()
+        }
+    }
+}
